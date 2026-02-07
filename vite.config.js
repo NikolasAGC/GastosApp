4307425f20404d9fb1d731dcbea8f9882c5e6afc
Nikolas <nikolas.magnet177@gmail.com>
2026-02-06 21:05:39 -0300

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-   base: '/GastosApp/',
+  base: '/GastosApp/',
   build: {
     outDir: 'dist',
     sourcemap: false,
+    assetsDir: 'assets'
   }
 })
